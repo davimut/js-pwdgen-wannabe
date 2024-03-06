a38bb44808f5619661074f7d717dc9c553046004
davimuto <davimuto@gmail.com>
2024-03-06 19:34:13 +0100

    add: generatore di password con numeri random

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+const userName = prompt('Inserisci il tuo nome !'); //string | null 
+
+console.log (userName);
+
+const surName = prompt('Inserisci il tuo cognome !'); //string | null 
+
+console.log (surName);
+
+ const color = prompt('Inserisci il tuo colore preferito !'); //string | null 
+
+ console.log (color);  //Stringss
+
+const randomNumber = Math.floor (Math.random(1)*100 ); // Number
+console.log ( randomNumber ) //string
+
+console.log ( userName + surName + color + randomNumber );  //string 
